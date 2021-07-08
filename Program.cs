aab981b3ba8105e5fd281cf910627c6ae7313cae
dasa777777 <75094391+dasa777777@users.noreply.github.com>
2021-07-08 21:00:01 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace cshello
+{
+    class Program
+    {
+        
+     
+            static void Main(string[] args)
+            {
+                Console.WriteLine("i am \talive\n");
+                Console.WriteLine("hello \nuser..");
+
+                int pocetStromov = 15;
+                double vyskaKlienta = 1.85;
+                string menoKlienta = "Peter Sagan";
+
+                Console.WriteLine("Pocet stromov je: " + pocetStromov);
+                Console.WriteLine("Vyska zamestnanca je: " + vyskaKlienta);
+                Console.WriteLine("Meno klienta je: " + menoKlienta);
+
+                int a = 5;
+                double b = 2.0;
+                double vysledok;
+
+                vysledok = a / b;
+                Console.WriteLine("Vysledok je: " + vysledok);
+                Console.ReadLine();
+
+            }
+        }
+    }
+
